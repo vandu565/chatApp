d5dcf3d505ae6213006c5e4b4026c0fc3775e66e
Vandana Yadav <vandu565@gmail.com>
2024-09-20 23:24:46 -0400

    Updating router,index,model

---
+++ routes/avatarRoute.js
@@ -0,0 +1,7 @@
+//avatarRoute.js
+const express = require('express');
+const avatarController = require('../controllers/avatarController')
+const router = express.Router();
+router.post("/", avatarController.avatarController);
+router.get("/all", avatarController.getAllAvatars);
+module.exports = router;
